e75bf4c5f44b3a617798ca5d39f85a21d72fccf1
konstantinterekhov <KonstantinTerekhov@coherentsolutions.com>
2018-02-02 14:15:54 +0300

    bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_202_093_456) do
+ActiveRecord::Schema.define(version: 20180202093456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
