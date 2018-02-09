2dbfebee229d67432e3f287fe072e3d870f6e0d7
konstantinterekhov <KonstantinTerekhov@coherentsolutions.com>
2018-02-09 14:45:17 +0300

    Updated UI and refactor controller

--- app/controllers/deposit_contracts_controller.rb
+++ app/controllers/deposit_contracts_controller.rb
@@ -1,67 +1,61 @@
 # frozen_string_literal: true
 
 class DepositContractsController < ApplicationController
-  before_action :set_deposit_contract, only: %i[show edit update destroy]
-
   respond_to :html
 
   # GET /deposit_contracts
   # GET /deposit_contracts.json
   def index
-    @deposit_contracts = DepositContract.all
+    @_resources = client.deposit_contracts.includes(:main_account, :current_account, deposit: %i[currency deposit_type])
     render layout: 'application_fluid'
   end
 
   # GET /deposit_contracts/1
   # GET /deposit_contracts/1.json
   def show
-    respond_with @deposit_contract
+    respond_with client, resource
   end
 
   # GET /deposit_contracts/new
   def new
-    @client = Client.find(params[:client_id])
-    @deposit_contract = DepositContract.new
-    respond_with @deposit_contract
+    respond_with client, @_resource = DepositContract.new
   end
 
   # GET /deposit_contracts/1/edit
-  def edit; end
+  def edit
+    respond_with client, resource
+  end
 
   # POST /deposit_contracts
   # POST /deposit_contracts.json
   def create
-    @deposit_contract = DepositContractService.new.create(deposit_contract_params)
-    respond_with @deposit_contract
+    respond_with client, @_resource = DepositContractService.new.create(client, deposit_contract_params)
   end
 
   # PATCH/PUT /deposit_contracts/1
   # PATCH/PUT /deposit_contracts/1.json
   def update
-    @deposit_contract.update(deposit_contract_params)
-
-    respond_with @deposit_contract
+    resource.update(deposit_contract_params)
+    respond_with client, resource
   end
 
   # DELETE /deposit_contracts/1
   # DELETE /deposit_contracts/1.json
   def destroy
-    @deposit_contract.destroy
-    respond_with @deposit_contract
-  end
-
-  def client
-    @client ||= Client.find(params[:client_id])
+    resource.destroy
+    respond_with resource
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_deposit_contract
-    @deposit_contract = DepositContract.find(params[:id])
+  def client
+    @_client ||= Client.find(params[:client_id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  def resource
+    @_resource ||= DepositContract.find(params[:id])
+  end
+
   def deposit_contract_params
     params.require(:deposit_contract).permit(:client_id, :deposit_id, :start_date, :end_date, :closed, :amount)
   end
