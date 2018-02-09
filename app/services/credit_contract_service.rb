a1e5addf4670ea21431c16bb7390a9aadead4c5b
Konstantin Terekhov <konstantin.terekhov@sportngin.com>
2018-02-09 23:36:27 +0300

    WIP: Credit Payments

--- app/services/credit_contract_service.rb
+++ app/services/credit_contract_service.rb
@@ -19,11 +19,14 @@ class CreditContractService
     TransactionService.new(credit_contract.credit.currency)
       .on_create_credit_contract(credit_contract, credit_contract.amount)
 
+
     credit_contract.save
     credit_contract
   end
 
-  private def create_credit_account(credit_contract, params = {})
+  private
+
+  def create_credit_account(credit_contract, params = {})
     account = Account.create({ client:       credit_contract.client,
                                currency:     credit_contract.credit.currency,
                                account_type: :credit,
@@ -32,5 +35,22 @@ class CreditContractService
                                real_amount:  0 }.merge(params))
     account.generate_number_and_pin
     account
+  end
+
+  def create_credit_payments(credit_contract)
+    if credit_contract.contract_type.annuity?
+      #proposed_amount = credit_contract.credit.rate / 1200 * (1 + credit_contract.credit.rate / 1200) ** credit_contract.credit.months / ((1 + credit_contract.credit.rate / 1200) ** credit_contract.credit.months - 1) * credit_contract.main_account.amount
+    else
+      #proposed_amount = credit_contract.credit.rate / 100 / 365 * (1 + credit_contract.credit.rate / 100) ** (credit_contract.end_date - credit_contract.start_date).days / ((1 + credit_contract.credit.rate / 100) ** (credit_contract.end_date - credit_contract.start_date).days - 1) * credit_contract.main_account.amount
+    end
+    number_of_months = (credit_contract.end_date.year * 12 + credit_contract.end_date.month) -
+      (credit_contract.start_date.year * 12 + credit_contractstart_date.month) - 1
+    number_of_months.times do |i|
+      (i + 1).months
+    end
+  end
+
+  def create_credit_payment(date, amount)
+
   end
 end
